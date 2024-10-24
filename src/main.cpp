b038566f47c82a2cd355e7105ea22f1ffcc8f745
IMCGKN <113937505+IMCGKN@users.noreply.github.com>
2024-10-24 11:58:23 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
+#include <App/IMCApplication.h>
 
+#include "IMCPrerequisites.h"
+
+int main()
+{
+	try
+	{
+		IMCApplication app;
+		app.OnCreate();
+	}
+	catch (const std::exception& e)
+	{
+		std::clog << e.what() << std::endl;
+	}
+}
