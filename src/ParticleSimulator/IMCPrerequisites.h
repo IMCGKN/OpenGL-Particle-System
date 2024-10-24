ccad1589826996a9e5d1e0146d42c85e259131a0
IMCGKN <113937505+IMCGKN@users.noreply.github.com>
2024-10-24 11:59:30 +0200

    Update IMCPrerequisites.h

--- src/ParticleSimulator/IMCPrerequisites.h
+++ src/ParticleSimulator/IMCPrerequisites.h
@@ -1,1 +1,18 @@
+#pragma once
 
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+
+#define IMC_ERROR(message)\
+{\
+	std::stringstream sstr;\
+	sstr << "IMC Error: " << message << std::endl;\
+	throw std::runtime_error(sstr.str());\
+}
+
+#define IMC_WARNING(message)\
+std::clog << "IMC Warning: " << message << std::endl;
+
+#define IMC_INFO(message)\
+std::clog << "IMC Info: " << message << std::endl;
